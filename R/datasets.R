d9001c6c89a2fd5c9916c6b899e3a76e16e2d065
fernote7 <fernote7@gmail.com>
2017-02-11 23:36:51 -0200

    reestructuring the entire package documentation.

---
+++ R/datasets.R
@@ -0,0 +1,20 @@
+#' A dataset used in the examples
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name data.q1
+#' @usage data(data.q1)
+#' @format A matrix with 100 rows and 20 variables
+NULL
+
+
+#' A dataset used in the examples
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name data.q2
+#' @usage data(data.q2)
+#' @format A list with 2 matrixes of 100 rows and 20 variables
+NULL
