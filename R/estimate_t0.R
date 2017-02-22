f4330ab23b5d632f9246e75f97ed90d746e0b0a8
gabrielrvsc <gabrielrvsc@yahoo.com.br>
2017-02-22 16:25:23 -0300

    Update estimate_t0.R

--- R/estimate_t0.R
+++ R/estimate_t0.R
@@ -45,40 +45,35 @@
 
 
 
-estimate_t0=function(data,fn,p.fn,start=0.3,end=0.95,treated.unity=1,lag=0,Xreg=NULL){
-  
-  for(i in 1:length(data)){
-    if(is.null(colnames(data[[i]]))){
-      colnames(data[[i]])=paste("V",i,"-U",1:ncol(data[[i]]),sep="")
-      cat("Variable names not informed. Automatic names supplied. \n")
+estimate_t0=function (data, fn, p.fn, start = 0.3, end = 0.95, treated.unity = 1, 
+          lag = 0, Xreg = NULL) 
+{
+  for (i in 1:length(data)) {
+    if (is.null(colnames(data[[i]]))) {
+      colnames(data[[i]]) = paste("V", i, "-U", 1:ncol(data[[i]]), 
+                                  sep = "")
     }
   }
-  
-  for(i in 1:length(data)){
-    aux=length(unique(colnames(data[[i]])))
-    k=ncol(data[[i]])
-    if(aux<k){
-      colnames(data[[i]])=paste("V",i,"-U",1:ncol(data[[i]]),sep="")
-      cat("Some variables had no name. Automatic names supplied. \n")
+  for (i in 1:length(data)) {
+    aux = length(unique(colnames(data[[i]])))
+    k = ncol(data[[i]])
+    if (aux < k) {
+      colnames(data[[i]]) = paste("V", i, "-U", 1:ncol(data[[i]]), 
+                                  sep = "")
     }
   }
-  
-  
-  T=nrow(data[[1]])
-  
-  starting.point=floor(start*T)
-  ending.point=floor(end*T)
-  
-  save.delta=matrix(0,T,length(data))
-  for(i in starting.point:ending.point){
-    m=fitARCO(data=data,fn=fn,p.fn = p.fn,treated.unity=treated.unity,lag=lag, t0=i,Xreg=Xreg,display = FALSE)
-    delta=m$delta[2]
-    save.delta[i,]=delta
+  T = nrow(data[[1]])
+  starting.point = floor(start * T)
+  ending.point = min(floor(end * T),nrow(data[[1]])-lag+1)
+  save.delta = matrix(0, T, length(data))
+  for (i in starting.point:ending.point) {
+    m = fitARCO(data = data, fn = fn, p.fn = p.fn, treated.unity = treated.unity, 
+                lag = lag, t0 = i, Xreg = Xreg, display = FALSE)
+    delta = m$delta[,2]
+    save.delta[i, ] = delta
   }
-  
-  delta.norm=sqrt(rowSums((save.delta)^2))
-  t0=which(delta.norm==max(delta.norm))
-  delta=delta.norm[t0]
-  
-  return(c("to"=t0,"delta.norm"=delta))
+  delta.norm = sqrt(rowSums((save.delta)^2))
+  t0 = which(delta.norm == max(delta.norm))
+  delta = delta.norm[t0]
+  return(c(to = t0, delta.norm = delta))
 }
