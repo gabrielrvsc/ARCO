f6743faf616b33c58f48f568c125468d95b80b53
gabrielrvsc <gabrielrvsc@yahoo.com.br>
2017-03-19 13:29:50 -0300

    update

--- R/datasets.R
+++ R/datasets.R
@@ -1,22 +1,24 @@
 #' A generated dataset used in the examples
 #' 
-#' This data contains 100 observations of 20 variables. Each variable is one ArCo unity. The intervention took place on the first unity at t0=51 by adding a constant equal to 3.
+#' This data contains 100 observations of 20 variables generated using the dgp from Carvalho, Masini and Medeiros (2016). Each variable is one ArCo unity. The intervention took place on the first unity at t0=51 by adding a constant equal to 0.628, which is one standard deviation of the treated unity variable before the intervention.
 #'
 #' @docType data
 #' @keywords datasets
 #' @name data.q1
 #' @usage data(data.q1)
 #' @format A matrix with 100 rows and 20 variables
+#' @references Carvalho, C., Masini, R., Medeiros, M. (2016) "ArCo: An Artificial Counterfactual Approach For High-Dimensional Panel Time-Series Data.".
 NULL
 
 
 #' A dataset used in the examples
 #'
-#' This data is a list with two matrixes, each have 100 observations and 6 variables. In the ArCo context each matrix is a variable and each variable in the matrixes is an unity. The intervention took place on the first unity at t0=51 by adding a constant of 15 and -10 on variables (matrixes) 1 and 2. 
+#' This data is a list with two matrixes, each have 100 observations and 6 variables. It was generatedthe using the dgp from Carvalho, Masini and Medeiros (2016). In the ArCo context, each matrix is a variable and each variable in the matrixes is an unity. The intervention took place on the first unity at t0=51 by adding constants of 0.840 and 0.511 (one standar deviation before the intervention) on variables (matrixes) 1 and 2. 
 #'
 #' @docType data
 #' @keywords datasets
 #' @name data.q2
 #' @usage data(data.q2)
 #' @format A list with 2 matrixes of 100 rows and 6 variables
+#' @references Carvalho, C., Masini, R., Medeiros, M. (2016) "ArCo: An Artificial Counterfactual Approach For High-Dimensional Panel Time-Series Data.".
 NULL
